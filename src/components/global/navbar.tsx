ae4f753356f5ec2fc8d9a0044a6f528bfdf1eb17
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-06-01 03:11:37 +0530

    added Navbar

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = async (props: Props) => {
+  return (
+    <header className=''>
+        Header
+    </header>
+  )
+}
+
+export default Navbar
