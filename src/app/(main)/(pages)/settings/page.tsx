7abd4b1d583d3ac11dc378ae803424bf71f49308
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-06-29 23:57:50 +0530

    Update page.tsx

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,10 +1,25 @@
 import ProfileForm from '@/components/forms/profile-form'
 import SectionHeader from '@/components/section-header'
 import React from 'react'
+import ProfilePicture from './_components/profile-picture'
+import { db } from '@/lib/db'
 
 type Props = {}
 
 const SettingsPage = (props: Props) => {
+
+    const removeProfileImage  = async ()=> {
+      'use server'
+      const response = await db.user.update({
+        where : {
+          clerkId : authUser.id
+        },
+        data : {
+          profileImage : ""
+        }
+      })
+      return response;
+    }
 
     //WIP: Wire up profile picture
   return (
@@ -19,6 +34,13 @@ const SettingsPage = (props: Props) => {
             <h2 className='font-bold text-3xl'>User Profile</h2>
             <p className='text-base text-white/50'>Update your profile here</p>
         </div>
+        {/* <ProfilePicture
+        onDelete={removeProfileImage}
+        userImage={user.profileImage || ''} 
+        onUpload={uploadProfileImage}
+        >
+
+        </ProfilePicture> */}
       <ProfileForm/>
       </div>
     </div>
