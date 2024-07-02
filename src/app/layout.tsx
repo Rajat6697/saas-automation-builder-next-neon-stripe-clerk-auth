53d58d087aaff0b53bdbc43ed68f8d7f913b00cd
Rajat6697 <74897722+Rajat6697@users.noreply.github.com>
2024-07-03 01:25:55 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider"; 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
 
 
 const font = DM_Sans({ subsets: ["latin"] });
@@ -17,8 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
     <html lang="en">
       <body className={font.className}>
+     
+
       <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -27,7 +39,10 @@ export default function RootLayout({
           >
         {children}
         </ThemeProvider>
+
         </body>
     </html>
+    </ClerkProvider>
+
   );
 }
